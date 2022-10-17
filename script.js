a1263ee9d9652aad33d28aacbf56ba0c5d123694
Mehdi Jussm <mehdi.jusseaume@gmail.com>
2022-10-17 13:50:22 +0200

    Add git ignore

--- script.js
+++ script.js
@@ -9,42 +9,50 @@ function getComputerChoice() { // Make the computer choose randomly between rock
 function playRound(playerSelection, cpuSelection) { // check who wins between computer or player
 
     if (playerSelection.toLowerCase() == "rock" && cpuSelection.toLowerCase() == "scissors") {
-        console.log("WIN !");
+//        console.log("WIN !");
         return "win"
     }
     else if (playerSelection.toLowerCase() == "paper" && cpuSelection.toLowerCase() == "rock") {
-        console.log("WIN !");
+//        console.log("WIN !");
         return "win"
     }
     else if (playerSelection.toLowerCase() == "scissors" && cpuSelection.toLowerCase() == "paper") {
-        console.log("WIN !");
+//        console.log("WIN !");
         return "win"
     }
     else if (playerSelection.toLowerCase() == cpuSelection.toLowerCase()) {
-        console.log("TIE.");
+//        console.log("TIE.");
         return "tie"
     }else {
-        console.log("LOOSE !");
+//        console.log("LOOSE !");
         return "lose"
     }
 }
 
 const btn = document.querySelectorAll('button');
-let btnPressed = '';
+let btnPressed;
+let cpu;
+
+const container = document.querySelector('#result');
+const content = document.createElement('div');
 
 btn.forEach((button) => {
     button.addEventListener('click', () => {
 
         btnPressed = button.className;
+        cpu = getComputerChoice();
+        let round = playRound(btnPressed, cpu);
 
-        const container = document.querySelector('#result');
-        const content = document.createElement('div');
-
-        content.classList.add('content');
-        content.textContent = `Computer choice: ${getComputerChoice()}`;
-        container.appendChild(content);
+        console.log("btn pressed: " + btnPressed)
+        content.innerText = `User choice: ${btnPressed}
+                Computer choice: ${cpu} 
+                Round: ${round}`;
     })
 });
+
+content.classList.add('content');
+console.log(`Content: ${content.innerText}`);
+container.appendChild(content);
 
 function game(btnPressed) {
     let roundResult = 0;
